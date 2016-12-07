657a77b557552429fb31f099d85a6589d1c1d1b5
Jan Niklas Boehm <mail@jnboehm.com>
2016-12-07 16:24:33 +0100

    Use bigger numbers in testbench

--- test/mult_tb.vhd
+++ test/mult_tb.vhd
@@ -8,8 +8,8 @@ use std.textio.all;
 
 entity mult_tb is
 --  Port ( );
-  generic(width_a : integer := 4;
-          width_b : integer := 4;
+  generic(width_a : integer := 16;
+          width_b : integer := 16;
           base    : integer := 4);
 
 end mult_tb;
@@ -24,8 +24,8 @@ architecture Behavioral of mult_tb is
 
   -- Output
 
-  signal a     : std_logic_vector(width_a - 1 downto 0) := "0111";-- := "0000001000000001";
-  signal b     : std_logic_vector(width_b - 1 downto 0) := "0011";-- := "0000001000000001";
+  signal a     : std_logic_vector(width_a - 1 downto 0) := "0000001000000001";
+  signal b     : std_logic_vector(width_b - 1 downto 0) := "0000001000000001";
   signal prd   : std_logic_vector(width_a + width_b - 1 downto 0);
   signal start : std_logic;
   signal ready : std_logic;
