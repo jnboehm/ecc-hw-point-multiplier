2b8b4df1f9d878097c224ceadfb77b934ba5cb49
Jan Niklas Boehm <mail@jnboehm.com>
2016-12-31 16:54:08 +0100

    Beautify module

--- src/subtraction.vhd
+++ src/subtraction.vhd
@@ -3,8 +3,8 @@ use IEEE.STD_LOGIC_1164.all;
 use IEEE.NUMERIC_STD.all;
 
 entity subtraction is
-  generic(width : integer := 256;
-          base  : integer := 32);
+  generic(width : integer := 396;
+          base  : integer := 18);
 
   port (a   : in  std_logic_vector(width - 1 downto 0);
         b   : in  std_logic_vector(width - 1 downto 0);
@@ -13,20 +13,20 @@ entity subtraction is
 end entity;
 
 architecture Behavioral of subtraction is
-  signal tmp : std_logic_vector(width - 1 downto 0);
-  signal tmp2 : std_logic_vector(width - 1 downto 0);
+  signal tmp     : std_logic_vector(width - 1 downto 0);
+  signal tmp2    : std_logic_vector(width - 1 downto 0);
   signal sum_tmp : std_logic_vector(width downto 0);
 begin
-  tmp <= std_logic_vector((width - 1 downto 0 => '1') xor b);
+  tmp  <= std_logic_vector((b'range => '1') xor b);
   tmp2 <= std_logic_vector(unsigned(tmp) + 1);
 
   adder : entity work.rc_adder (Behavioral)
-     generic map (width => width,
-                  base => base)
-      port map (a    => a,
-                b    => tmp2,
-                cin => cin,
-                s    => sum_tmp);
+    generic map (width => width,
+                 base  => base)
+    port map (a   => a,
+              b   => tmp2,
+              cin => cin,
+              s   => sum_tmp);
 
   s <= sum_tmp(width - 1 downto 0);
 
